6237354fa89a5713e4e4e34ed6838b2cf6ef36cf
Goldenprevue <goldenprevue@gmail.com>
2023-03-15 21:02:00 +0900

    [ADD] Render Page create

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,38 @@
 'use client';
+import Image from 'next/image';
 import React from 'react';
-
+import styles from './page.module.css';
+import sectionseven from 'assets/sectionseven.png';
 export default function Home() {
   return (
     <div>
-      <h1>렌더링 페이지d</h1>
+      <div className={styles.container}>
+        <section className={styles.section_one}>
+          <article></article>
+        </section>
+        <section className={styles.section_two}>
+          <article></article>
+          <article></article>
+        </section>
+        <section className={styles.section_three}>
+          <article></article>
+        </section>
+        <section className={styles.section_four}>
+          <article></article>
+        </section>
+        <section className={styles.section_five}>
+          <article></article>
+        </section>
+        <section className={styles.section_six}>
+          <article></article>
+        </section>
+        <section className={styles.section_seven}>
+          <article>sdf</article>
+        </section>
+        <section className={styles.section_eight}>
+          <article></article>
+        </section>
+      </div>
     </div>
   );
 }
