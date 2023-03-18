c46fab076f083ace1588a60ea9708ab96d767b3b
Goldenprevue <goldenprevue@gmail.com>
2023-03-18 21:59:23 +0900

    merge commit

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
 import RootLayout from './layout';
-import {
-  Hydrate,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
+import { Hydrate } from '@tanstack/react-query';
 
 // const queryClient = new QueryClient();
 
 export default function APP({ Component, pageProps }: AppProps) {
-  const [queryClient] = React.useState(() => new QueryClient());
-
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <Hydrate state={pageProps.dehydratedState}>
-          <Component {...pageProps} />
-        </Hydrate>
-        <ReactQueryDevtools initialIsOpen={true} />
-      </QueryClientProvider>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
     </>
   );
 }
