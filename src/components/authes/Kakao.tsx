29a3646d831c8a68c084f22acb063779f6319f8c
Goldenprevue <goldenprevue@gmail.com>
2023-03-19 22:57:40 +0900

    [EDIT] kakao login && authes 수정

--- src/components/authes/Kakao.tsx
+++ src/components/authes/Kakao.tsx
@@ -1,6 +1,6 @@
-'use client';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const KakaoCallback = () => {
   const router = useRouter();
@@ -8,16 +8,16 @@ const KakaoCallback = () => {
   console.log(router);
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
-    console.log('urlParams', urlParams);
-    const code = urlParams.get('code');
-    console.log('code', code);
+    console.log("urlParams", urlParams);
+    const code = urlParams.get("code");
+    console.log("code", code);
 
     if (code) {
       // 백엔드 서버에 액세스 토큰과 리프레시 토큰을 요청합니다.
-      fetch(`${process.env.NEXT_PUBLIC_API}/auth/kakao/callback?code=${code}`, {
-        method: 'GET',
+      fetch(`${process.env.NEXT_PUBLIC_API}auth/kakao/callback?code=${code}`, {
+        method: "GET",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
       })
         .then((response) => {
@@ -28,10 +28,10 @@ const KakaoCallback = () => {
           // 쿠키에 토큰을 저장하거나 필요한 작업 수행
           document.cookie = `access_token=${access_token}; secure=false;`;
           document.cookie = `refresh_token=${refresh_token}; secure=false;`;
-          console.log('access_token', access_token);
-          console.log('refresh_token', refresh_token);
+          console.log("access_token", access_token);
+          console.log("refresh_token", refresh_token);
           // 메인 페이지로 리다이렉션
-          router.push('/main');
+          router.push("/main");
         })
         .catch((error) => {
           // 에러 처리
--- src/components/loginPage/LoginBox.tsx
+++ src/components/loginPage/LoginBox.tsx
@@ -1,19 +1,19 @@
-import Image from 'next/image';
-import styles from './LoginBox.module.css';
-import kakao_login from 'assets/kakao_login.png';
-import naver_login from 'assets/naver_login.png';
-import r_google from 'assets/r_google.png';
+import Image from "next/image";
+import styles from "./LoginBox.module.css";
+import kakao_login from "assets/kakao_login.png";
+import naver_login from "assets/naver_login.png";
+import r_google from "assets/r_google.png";
 const LoginBox = () => {
-  const kakaoUrl = `${process.env.NEXT_PUBLIC_API}/auth/kakao`;
+  const kakaoUrl = `${process.env.NEXT_PUBLIC_API}auth/kakao`;
   const onClickKakaoBtn = () => {
     window.location.href = kakaoUrl;
   };
   const onClickNaverBtn = () => {
-    alert('개발중입니다..!');
+    alert("개발중입니다..!");
   };
 
   const onClickGoogleBtn = () => {
-    alert('개발중입니다...!');
+    alert("개발중입니다...!");
   };
   return (
     <div className={styles.container}>
@@ -37,7 +37,7 @@ const LoginBox = () => {
           </button>
         </div>
         <div>
-          <Image src={r_google} alt={''} width={15} height={15} />
+          <Image src={r_google} alt={""} width={15} height={15} />
           <button className={styles.google_btn} onClick={onClickGoogleBtn}>
             구글로 1초만에 시작하기
           </button>
