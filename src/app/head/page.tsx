d16100cc9e41d6f264ec3f7625a02e48c76b4515
crystal <oj3339@naver.com>
2023-03-19 22:09:44 +0900

    [EDIT] mobile error page

---
+++ src/app/head/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Head from "next/head";
+
+export default function page() {
+  return (
+    <>
+      <Head>
+        <meta property="og:image" content="../../../public/og-image.png" />
+      </Head>
+    </>
+  );
+}
