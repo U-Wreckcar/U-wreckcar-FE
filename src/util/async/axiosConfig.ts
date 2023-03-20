d15e02940c6f75b8424586286b25ab276ee189a9
Goldenprevue <goldenprevue@gmail.com>
2023-03-20 20:51:35 +0900

    font 수정

--- src/util/async/axiosConfig.ts
+++ src/util/async/axiosConfig.ts
@@ -1,20 +1,19 @@
 import axios from 'axios';
 import { getCookie } from './Cookie';
 
-const refresh_token = getCookie("refresh_token")
-const access_token = getCookie("access_token")
+const refresh_token = getCookie('refresh_token');
+const access_token = getCookie('access_token');
 
 const instance = axios.create({
-
   withCredentials: true,
 
   baseURL: process.env.NEXT_PUBLIC_API,
   timeout: 3000,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
     Authorization: `Bearer ${access_token}`,
-    "X-Refresh-Token": `Bearer ${refresh_token}`,
-    "Cache-Control": "no-cache, no-store, must-revalidate",
+    'X-Refresh-Token': `Bearer ${refresh_token}`,
+    'Cache-Control': 'no-cache, no-store, must-revalidate',
   },
 });
 
