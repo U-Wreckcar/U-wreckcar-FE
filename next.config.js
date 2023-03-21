04771a1816d044cec129db8b74e30922f6350be5
Goldenprevue <goldenprevue@gmail.com>
2023-03-22 00:26:24 +0900

    [ERORRO] 이유없이 404에러 버셀 문제 인듯

--- next.config.js
+++ next.config.js
@@ -10,15 +10,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
-module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'k.kakaocdn.net',
-        port: '',
-      },
-    ],
-  },
-};
