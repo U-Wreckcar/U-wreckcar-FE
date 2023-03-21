7cf3a7ece6a94802aff419f2bd41a084da0ad6f4
Goldenprevue <goldenprevue@gmail.com>
2023-03-22 00:00:51 +0900

    [ADD] 헤더 프로필 이미지 && 유저페이지 구현

--- next.config.js
+++ next.config.js
@@ -3,6 +3,22 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    domains: ['localhost', '*'],
+  },
+  reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'k.kakaocdn.net',
+        port: '',
+      },
+    ],
+  },
+};
