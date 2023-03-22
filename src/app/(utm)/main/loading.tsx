ee26b546b2c72d78732af7612491acf413a0c610
crystal <oj3339@naver.com>
2023-03-23 08:23:40 +0900

    [ADD] main page loading

---
+++ src/app/(utm)/main/loading.tsx
@@ -0,0 +1,10 @@
+import CircularUnderLoad from "@/components/CircularUnderLoad"
+import React from "react"
+
+export default function Mainloading() {
+  return (
+    <div>
+      <CircularUnderLoad />
+    </div>
+  )
+}
