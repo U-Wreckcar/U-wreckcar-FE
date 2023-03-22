ae2f3645ba5e41d925c048d63dbee4a217f34d62
Goldenprevue <goldenprevue@gmail.com>
2023-03-23 00:20:39 +0900

    [ADD] 레이아웃 파일 구글 아날리틱스 주입

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE});
+              gtag('config', 'G - N10RX7RLWY');
             `,
           }}
         />
