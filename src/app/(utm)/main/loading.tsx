1d46aecd95d4b27a7a9120bdebf65bab168dd1e9
crystal <oj3339@naver.com>
2023-03-23 09:24:45 +0900

    [EDIT] 날짜 필터 초기화

--- src/app/(utm)/main/loading.tsx
+++ src/app/(utm)/main/loading.tsx
@@ -2,9 +2,5 @@ import CircularUnderLoad from "@/components/CircularUnderLoad"
 import React from "react"
 
 export default function Mainloading() {
-  return (
-    <div>
-      <CircularUnderLoad />
-    </div>
-  )
+  return <div>로딩중...</div>
 }
