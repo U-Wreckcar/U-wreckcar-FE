c1be267f325e8012daae239c6540aeab39c6c195
Goldenprevue <goldenprevue@gmail.com>
2023-03-25 00:18:56 +0900

    [ADD] create page url생성 placeholder 값 수정

--- src/components/createPage/CreateCopyBox.tsx
+++ src/components/createPage/CreateCopyBox.tsx
@@ -23,29 +23,31 @@ export const CreateCopyBox: React.FC<PropsType> = ({ resUTM }) => {
         <h3 className={styles.utm_shorten_url}>Shorten URL</h3>
       </div>
       <div>
-        {resUTM?.map((i: any, idx: number) => (
-          <div key={i.utm_id}>
-            <div className={styles.list_box}>
-              <div className={styles.copy_box_number}>{idx + 1}</div>
-              <div className={styles.full_box}>
-                <div className={styles.copy_box_full}>
-                  <div className={styles.full_utm}>
-                    <div className={styles.text_full}>{i.full_url}</div>
+        <div className={styles.list_container}>
+          {resUTM?.map((i: any, idx: number) => (
+            <div key={i.utm_id}>
+              <div className={styles.list_box}>
+                <div className={styles.copy_box_number}>{idx + 1}</div>
+                <div className={styles.full_box}>
+                  <div className={styles.copy_box_full}>
+                    <div className={styles.full_utm}>
+                      <div className={styles.text_full}>{i.full_url}</div>
+                    </div>
+                    <CreateCopyButton text={i.full_url} />
                   </div>
-                  <CreateCopyButton text={i.full_url} />
                 </div>
-              </div>
-              <div className={styles.short_box}>
-                <div className={styles.copy_box_short}>
-                  <div className={styles.short_utm}>
-                    <div className={styles.text_full}>{i.shorten_url}</div>
+                <div className={styles.short_box}>
+                  <div className={styles.copy_box_short}>
+                    <div className={styles.short_utm}>
+                      <div className={styles.text_full}>{i.shorten_url}</div>
+                    </div>
+                    <CreateCopyButton text={i.shorten_url} />
                   </div>
-                  <CreateCopyButton text={i.shorten_url} />
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   )
