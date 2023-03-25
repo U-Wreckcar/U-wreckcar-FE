7c9a72edabe9d2eee86dae68df51661221059903
Goldenprevue <goldenprevue@gmail.com>
2023-03-25 15:10:31 +0900

    [EDIT] OG이미지

--- src/app/og.png/page.tsx
+++ src/app/og.png/page.tsx
@@ -1,15 +1,10 @@
-import Image from 'next/image';
-import React from 'react';
-import ogimg from 'assets/ogimg.png';
+import Image from "next/image"
+import React from "react"
+import ogimage from "assets/ogimage.png"
 export default function OGImage() {
   return (
     <>
-      <Image
-        src={ogimg}
-        alt='og-image'
-        width={800}
-        height={600}
-      />
+      <Image src={ogimage} alt='og-image' width={1200} height={630} />
     </>
-  );
+  )
 }
--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -18,9 +18,6 @@ export default function () {
           color: "white",
           width: "100%",
           height: "100%",
-          // display: "flex",
-          // alignItems: "center",
-          // justifyContent: "center",
         }}></div>
     ),
     {
