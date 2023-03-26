01e05012173b29423557704288f100f76854a297
Goldenprevue <goldenprevue@gmail.com>
2023-03-26 23:35:24 +0900

    ✨[ADD] 로그인 페이지 카카오톡 Noti 알럿 추가

--- src/components/loginPage/LoginBox.tsx
+++ src/components/loginPage/LoginBox.tsx
@@ -4,10 +4,12 @@ import kakao_login from "assets/kakao_login.png"
 import naver_login from "assets/naver_login.png"
 import r_google from "assets/r_google.png"
 import { getCookie } from "@/util/async/Cookie"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { redirect } from "next/navigation"
+import { Modal } from "@/shared/modal/Modal"
 
 const LoginBox = () => {
+  const [noti, setNoti] = useState(true)
   useEffect(() => {
     const cookie = getCookie("access_token")
     if (cookie) {
@@ -28,6 +30,22 @@ const LoginBox = () => {
   }
   return (
     <div className={styles.container}>
+      {noti && (
+        <Modal
+          x={500}
+          y={250}
+          setNoti={setNoti}
+          modalTitle={"카카오톡 로그인하기"}
+          context={
+            "현재 회원가입 시 전체 동의하기에 체크해야 유렉카의 \n 모든 기능을 정상적으로 이용하실 수 있습니다"
+          }
+          contextSeconde={
+            "*프로필 사진이 제대로 나오지 않거나 기능이 정상적으로 작동하지 않을 시에 가이드를 참고해주세요."
+          }
+          confirmButtonName={"확인"}
+        />
+      )}
+
       <div>
         <h1 className={styles.title}>Login</h1>
         <p className={styles.content}>
@@ -36,13 +54,13 @@ const LoginBox = () => {
       </div>
       <div>
         <div>
-          <Image src={kakao_login} alt="" width={15} height={15} />
+          <Image src={kakao_login} alt='' width={15} height={15} />
           <button className={styles.kakao_btn} onClick={onClickKakaoBtn}>
             카카오로 1초만에 시작하기
           </button>
         </div>
         <div>
-          <Image src={naver_login} alt="" width={15} height={15} />
+          <Image src={naver_login} alt='' width={15} height={15} />
           <button className={styles.naver_btn} onClick={onClickNaverBtn}>
             네이버로 1초만에 시작하기
           </button>
@@ -53,6 +71,10 @@ const LoginBox = () => {
             구글로 1초만에 시작하기
           </button>
         </div>
+        <p className={styles.login_decription}>
+          ※ 현재 회원가입 시 전체 동의하기에 체크해야 유렉카 기능을 정상적으로
+          이용하실 수 있습니다.
+        </p>
       </div>
     </div>
   )
