93cdf56621e571d71093409576c4332fb3617203
Goldenprevue <goldenprevue@gmail.com>
2023-04-02 01:50:12 +0900

    [EDIT] setClientHeaders token으로 변경

--- src/components/authes/Kakao.tsx
+++ src/components/authes/Kakao.tsx
@@ -25,7 +25,7 @@ const KakaoCallback = () => {
         .then(async (response) => {
           const res = await response.json()
           setCookie("refresh_token", res.token)
-          setClientHeaders(res.refresh_token)
+          setClientHeaders(res.token)
         })
         .then(() => {
           router.push("/main")
