d291610bbc612da0c86076744ee241f31f8f69ba
crystal <oj3339@naver.com>
2023-04-18 20:10:25 +0900

    :recycle: [EDIT] main page와 table 분리 작업중

--- src/app/(utm)/main/page.tsx
+++ src/app/(utm)/main/page.tsx
@@ -1,10 +1,14 @@
+"use client"
+
 import MainPageComponent from "src/components/mainPage/MainPage"
 import React from "react"
+import { Provider } from "react-redux"
+import store from "src/redux/store/store"
 
 export default function MainPage() {
   return (
-    <>
+    <Provider store={store}>
       <MainPageComponent />
-    </>
+    </Provider>
   )
 }
