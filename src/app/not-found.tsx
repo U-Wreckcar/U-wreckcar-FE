8f9ec6bc61066830b49bb1b38e93412e26c973f9
Goldenprevue <goldenprevue@gmail.com>
2023-04-28 21:04:19 +0900

    :recycle: 파일정리

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,21 +1,16 @@
-import Image from "next/image";
-import React from "react";
-import styles from "./404.module.css";
-import notfoundpage from "public/assets/img/notfoundpage.png";
-import NotFoundCompo from "../components/notFound/NotFoundCompo";
 import Link from "next/link";
+import Image from "next/image";
+import styles from "./not-found.module.css";
+import notfoundpage from "public/assets/img/notfoundpage.png";
 export default function NotFound() {
-   // const router = useRouter();
-   // const goBack = () => {
-   //    router.back();
-   // };
    return (
       <>
          <div className={styles.container} style={{ textAlign: "center", marginTop: "250px" }}>
-            {/* <NotFoundCompo /> */}
+            <Link href={"/login"}>
             <h2> 404 Not Found 잘못된 URL</h2>
             <h3>URL을 확인하세요 그리고 새로고침 해주세요</h3>
-            <Link href={"/login"}>{/* <Image src={notfoundpage} alt="button" width={250} height={50} /> */}</Link>
+               <Image src={notfoundpage} alt="button" width={250} height={50} />
+            </Link>
          </div>
       </>
    );
