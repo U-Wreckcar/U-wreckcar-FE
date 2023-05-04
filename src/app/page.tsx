550449e9223fa4137ed77c79356603865487c1d1
Goldenprevue <goldenprevue@gmail.com>
2023-05-05 06:45:24 +0900

    :recycle: section sub && try test 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,18 +25,9 @@ export default function Home() {
   return (
     <>
       <div>
-        <div className={styles.container}>
+        <div>
           <HomeOne />
-          <section className={styles.l_section_one_two}>
-            <article>
-              <Image className={styles.web_sub} src={sub_img} alt="Img" />
-              <Image className={styles.m_sub} src={m_section_one_two} alt="Img" />
-              <Image className={styles.sub_child} src={sub_child} alt="Img" width={1608} height={400} />
-            </article>
-          </section>
-          <section className={styles.try_section}>
-            <TryUTM />
-          </section>
+
           <section className={styles.l_section_two}>
             <article>
               <div className={styles.text_box}>
--- src/components/homePage/HomeLayout.tsx
+++ src/components/homePage/HomeLayout.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import HomeOne from "./HomeOne";
 import styles from "./Home.module.css";
+import HomeOneTwo from "./HomeOneTwo";
+import HomeTwoTry from "./HomeTwoTry";
 export default function HomeLayout() {
   return (
     <div className={styles.container}>
       <HomeOne />
+      <HomeOneTwo />
+      <HomeTwoTry />
     </div>
   );
 }
