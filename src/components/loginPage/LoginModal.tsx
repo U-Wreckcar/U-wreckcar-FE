9db770388ae6028d32354e83b57850efd88d803d
Goldenprevue <goldenprevue@gmail.com>
2023-05-25 23:16:05 +0900

    줄바꿈 에러 해결

--- src/components/loginPage/LoginModal.tsx
+++ src/components/loginPage/LoginModal.tsx
@@ -19,21 +19,24 @@ const LoginModal = ({ isOpen, onRequestClose }: any) => {
   };
 
   return (
-    <Modal isOpen={isOpen} style={customStyles} onRequestClose={onRequestClose}>
-      <div className={styles.modal_container}>
-        <div className={styles.underLine}>
-          {/* <h4>안내</h4> */}
-          <h4 style={{ color: "red" }}>긴급 공지</h4>
-        </div>
-        <div className={styles.modal_wrap}>
-          <div style={{ marginTop: "70px" }}>
-            <p>서버 이슈로 인하여 현재 유렉카 서비스가 정상적으로 운영</p>
-            <p>되지 않고 있습니다. 필요한 UTM목록이 있거나 기타 문의사항은</p>
-            <p>오픈채팅으로 연락주시면 빠르게 대처해드리도록 하겠습니다.</p>
-            <p>서비스 이용에 불편함을 드려 죄송합니다.</p>
-            <h3 style={{ textAlign: "center", margin: "40px 40px 70px 0" }}>- TEAM 유렉카 -</h3>
+    <>
+      <Modal isOpen={isOpen} style={customStyles} onRequestClose={onRequestClose}>
+        <div className={styles.modal_container}>
+          <div className={styles.underLine}>
+            {/* <h4>안내</h4> */}
+            <h4 style={{ color: "red" }}>긴급 공지</h4>
           </div>
-          {/* <div>
+          <div className={styles.modal_wrap}>
+            <div style={{ marginTop: "70px" }}>
+              <p>서버 이슈로 인하여 현재 유렉카 서비스가 정상적으로 운영</p>
+
+              <p>되지 않고 있습니다. 필요한 UTM목록이 있거나 기타 문의사항은</p>
+
+              <p>오픈채팅으로 연락주시면 빠르게 대처해드리도록 하겠습니다.</p>
+              <p>서비스 이용에 불편함을 드려 죄송합니다.</p>
+              <h3 style={{ textAlign: "center", margin: "40px 40px 70px 0" }}>- TEAM 유렉카 -</h3>
+            </div>
+            {/* <div>
             <h2 className={styles.modal_title}>회원가입 방식 변경 안내</h2>
           </div>
           <div>
@@ -48,17 +51,18 @@ const LoginModal = ({ isOpen, onRequestClose }: any) => {
             <p>자세한 사항은 공지사항 확인 부탁드립니다.</p>
           </div> */}
 
-          <div className={styles.button_box}>
-            <button className={styles.noti_button} onClick={onClickNoti}>
-              공지사항 보기
-            </button>
-            <button className={styles.confirm_button} onClick={onRequestClose}>
-              확인
-            </button>
+            <div className={styles.button_box}>
+              <button className={styles.noti_button} onClick={onClickNoti}>
+                공지사항보기
+              </button>
+              <button className={styles.confirm_button} onClick={onRequestClose}>
+                확인
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </Modal>
+      </Modal>
+    </>
   );
 };
 
