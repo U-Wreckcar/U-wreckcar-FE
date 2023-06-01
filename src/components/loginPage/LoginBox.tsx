c4b1ec6473b941f6e11477fc264b82b5323be926
crystal025 <109053875+crystal025@users.noreply.github.com>
2023-06-01 21:56:40 +0900

    :zap: kakao login remove

--- src/components/loginPage/LoginBox.tsx
+++ src/components/loginPage/LoginBox.tsx
@@ -12,42 +12,42 @@ import LocalLogin from "./LocalLogin";
 import { Alert } from "@mui/material";
 import LoginModal from "./LoginModal";
 const LoginBox = ({ router }: any) => {
-   const [local, setLocal] = useState(false);
-   const [alert, setAlert] = useState(false);
-   const [noti, setNoti] = useState(true);
+  const [local, setLocal] = useState(false);
+  const [alert, setAlert] = useState(false);
+  const [noti, setNoti] = useState(true);
 
-   useEffect(() => {
-      const cookie = getCookie("access_token");
-      if (cookie) {
-         redirect("/main");
-      }
-   }, []);
+  useEffect(() => {
+    const cookie = getCookie("access_token");
+    if (cookie) {
+      redirect("/main");
+    }
+  }, []);
 
-   const kakaoUrl = `${process.env.NEXT_PUBLIC_API}auth/kakao`;
-   const onClickKakaoBtn = () => {
-      window.location.href = kakaoUrl;
-   };
-   const onClickNaverBtn = () => {
-      setAlert(true);
-      // alert("개발중입니다..!")
-   };
+  const kakaoUrl = `${process.env.NEXT_PUBLIC_API}auth/kakao`;
+  const onClickKakaoBtn = () => {
+    window.location.href = kakaoUrl;
+  };
+  const onClickNaverBtn = () => {
+    setAlert(true);
+    // alert("개발중입니다..!")
+  };
 
-   const onClickGoogleBtn = () => {
-      setAlert(true);
-      // alert("개발중입니다...!")
-   };
+  const onClickGoogleBtn = () => {
+    setAlert(true);
+    // alert("개발중입니다...!")
+  };
 
-   useEffect(() => {
-      if (alert) {
-         setTimeout(() => {
-            setAlert(false);
-         }, 2000);
-      }
-   }, [alert]);
+  useEffect(() => {
+    if (alert) {
+      setTimeout(() => {
+        setAlert(false);
+      }, 2000);
+    }
+  }, [alert]);
 
-   return (
-      <div className={styles.container}>
-         {/* {noti && (
+  return (
+    <div className={styles.container}>
+      {/* {noti && (
         <Modal
           x={500}
           y={464}
@@ -62,25 +62,25 @@ const LoginBox = ({ router }: any) => {
           confirmButtonName={"확인"}
         />
       )} */}
-         {noti && <LoginModal isOpen={noti} onRequestClose={() => setNoti(false)} />}
+      {noti && <LoginModal isOpen={noti} onRequestClose={() => setNoti(false)} />}
 
-         <h1 className={styles.title}>Login</h1>
-         <p className={styles.content}>U렉카와 함께 쉽고 빠른 업무를 느껴보세요!</p>
-         {alert && (
-            <Alert className={styles.alert} severity="warning">
-               아직 개발 중입니다...! <strong>카카오로 바로 시작해보세요!</strong>
-            </Alert>
-         )}
+      <h1 className={styles.title}>Login</h1>
+      <p className={styles.content}>U렉카와 함께 쉽고 빠른 업무를 느껴보세요!</p>
+      {alert && (
+        <Alert className={styles.alert} severity="warning">
+          아직 개발 중입니다...! <strong>카카오로 바로 시작해보세요!</strong>
+        </Alert>
+      )}
 
-         <LocalLogin setLocal={setLocal} />
-         <button className={styles.kakao_btn} onClick={onClickKakaoBtn}>
+      <LocalLogin setLocal={setLocal} />
+      {/* <button className={styles.kakao_btn} onClick={onClickKakaoBtn}>
             <p>
                <Image className={styles.kakao_img} src={kakao_login} alt="" />
                <span> 카카오로 시작하기</span>
             </p>
-         </button>
+         </button> */}
 
-         {/* <div>
+      {/* <div>
                 <Image src={naver_login} alt="" width={15} height={15} />
                 <button className={styles.naver_btn} onClick={onClickNaverBtn}>
                   네이버로 1초만에 시작하기
@@ -95,8 +95,8 @@ const LoginBox = ({ router }: any) => {
                   구글로 1초만에 시작하기
                 </button>
               </div> */}
-      </div>
-   );
+    </div>
+  );
 };
 
 export default LoginBox;
